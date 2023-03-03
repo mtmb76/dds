a7eb2247f88a1ecc945ee5b53c2ffed55bcb4245
Marcelo Tavares <mtmb76@gmail.com>
2023-03-03 12:49:46 -0300

    Correções de Layout e fluxo entre telas. Versão pré-release.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -62,14 +62,6 @@
 
 @endsection
 
-<script>
-    var purecookieTitle="Política de Cookies",
-        purecookieDesc="Utilizamos alguns cookies para guardar informações de acesso. Clicando em 'Concordo', você aceita nossa política de proteção de seus dados.",
-        purecookieLink='<a href="https://www.mpf.mp.br/servicos/lgpd/o-que-e-a-lgpd" target="_blank">Saiba mais</a>',
-        purecookieButton="Concordo ";
-    function pureFadeIn(e,o){var i=document.getElementById(e);i.style.opacity=0,i.style.display=o||"block",function e(){var o=parseFloat(i.style.opacity);(o+=.02)>1||(i.style.opacity=o,requestAnimationFrame(e))}()}function pureFadeOut(e){var o=document.getElementById(e);o.style.opacity=1,function e(){(o.style.opacity-=.02)<0?o.style.display="none":requestAnimationFrame(e)}()}function setCookie(e,o,i){var t="";if(i){var n=new Date;n.setTime(n.getTime()+24*i*60*60*1e3),t="; expires="+n.toUTCString()}document.cookie=e+"="+(o||"")+t+"; path=/"}function getCookie(e){for(var o=e+"=",i=document.cookie.split(";"),t=0;t<i.length;t++){for(var n=i[t];" "==n.charAt(0);)n=n.substring(1,n.length);if(0==n.indexOf(o))return n.substring(o.length,n.length)}return null}function eraseCookie(e){document.cookie=e+"=; Max-Age=-99999999;"}function cookieConsent(){getCookie("purecookieDismiss")||(document.body.innerHTML+='<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>'+purecookieTitle+'</a></div><div class="cookieDesc"><p>'+purecookieDesc+" "+purecookieLink+'</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">'+purecookieButton+"</a></div></div>",pureFadeIn("cookieConsentContainer"))}function purecookieDismiss(){setCookie("purecookieDismiss","1",7),pureFadeOut("cookieConsentContainer")}window.onload=function(){cookieConsent()};
-</script>
-
 <style>
     input[type="text"]
     {
@@ -101,68 +93,5 @@
 
     body {
         margin: 0px;
-    }
-
-    .cookieConsentContainer{
-        z-index: 999;
-        width: 350px;
-        min-height: 20px;
-        box-sizing: border-box;
-        padding:30px 30px 30px 30px;
-        background-color: #232323;
-        color: white;
-        overflow:hidden;
-        position:fixed;
-        bottom:30px;
-        right:30px;
-        display:none;
-        border-radius: 20px;
-    }
-    .cookieConsentContainer .cookieTitle a{
-        font-family:OpenSans,arial,sans-serif;
-        color: white;
-        font-size:22px;
-        line-height:20px;
-        display:block;
-    }
-    .cookieConsentContainer .cookieDesc p{
-        margin: 0;
-        padding: 0;
-        font-family: OpenSans,arial,sans-serif;
-        color: white;
-        font-size:13px;
-        line-height:20px;
-        display:block;
-        margin-top:10px;
-    }
-    .cookieConsentContainer .cookieDesc a{
-        font-family:OpenSans,arial,sans-serif;
-        color: white;
-        text-decoration:underline;
-    }
-    .cookieConsentContainer .cookieButton a{
-        display:inline-block;
-        font-family:OpenSans,arial,sans-serif;
-        color: white;
-        font-size:14px;
-        font-weight:700;
-        margin-top:14px;
-        background: black;
-        box-sizing: border-box;
-        padding: 15px 24px;
-        text-align: center;
-        transition: background .3s;
-        border-radius: 20px;
-    }
-    .cookieConsentContainer .cookieButton a:hover{
-        cursor:pointer;
-        background:#3e9b67;
-    }
-    @media (max-width:980px){
-        .cookieConsentContainer{
-            bottom:0!important;
-            left:0!important;
-            width:100%!important;
-        }
     }
 </style>
