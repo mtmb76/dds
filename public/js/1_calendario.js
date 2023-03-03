4310c2600a604b75792fd3b01aea53cc11c6e35b
Marcelo Tavares <mtmb76@gmail.com>
2023-03-03 19:19:39 -0300

    Instalação do octane com swoole

--- public/js/1_calendario.js
+++ public/js/1_calendario.js
@@ -8,7 +8,7 @@
                     months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
                     days: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
                     events: [
-                    {startDate: "2023-03-04",  endDate: "2023-03-04",  summary:"<a style='font-size:14px; color:#eee089;' href='http://localhost:8000/evento/view/7'>Evento #7</a><br>Turno: 1º<br>Setor: Administrativo<br>Tema: Gray"},
+                    
                     ],
                 });
             });
